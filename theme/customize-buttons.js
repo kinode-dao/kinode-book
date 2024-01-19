0607545fefba6efb7d1128a07e504f240e7011e4
habsul-rignyr <joshamy@gmail.com>
2024-01-18 18:09:34 -0600

    Set default to Light

--- theme/customize-buttons.js
+++ theme/customize-buttons.js
@@ -1,7 +1,7 @@
 document.addEventListener("DOMContentLoaded", () => {
   // Renaming the theme buttons based on their IDs
   const lightButton = document.getElementById("light");
-  if (lightButton) lightButton.textContent = "Light"; // Keeping the name 'Light'
+  if (lightButton) lightButton.textContent = "Meadow"; // Keeping the name 'Light'
 
   const rustButton = document.getElementById("rust");
   if (rustButton) rustButton.textContent = "Harmony"; // New name for 'Rust'
@@ -13,5 +13,5 @@ document.addEventListener("DOMContentLoaded", () => {
   if (ayuButton) ayuButton.textContent = "Wynn"; // New name for 'Ayu'
 
   const navyButton = document.getElementById("navy");
-  if (navyButton) navyButton.textContent = "Meadow"; // New name for 'Navy'
+  if (navyButton) navyButton.textContent = "Light"; // New name for 'Navy'
 });
