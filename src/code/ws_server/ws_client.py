82a30aa65a8a5bb1c8e2bd3af57569f93d7d0dd4
hosted-fornet <hosted-fornet@protonmail.com>
2024-06-13 16:33:59 -0700

    outside: fix ws_servers client

--- src/code/ws_server/ws_client.py
+++ src/code/ws_server/ws_client.py
@@ -7,7 +7,7 @@ async def websocket_client():
     uri = "ws://localhost:8080/ws_server:ws_server:template.os"
 
     # Connect to the WebSocket server
-    async with websockets.connect(uri) as websocket:
+    async with websockets.connect(uri, ping_interval=None) as websocket:
         # Wait for a message from the server
         message = await websocket.recv()
         print(f"Received from server: {message}")
